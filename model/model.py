0706e4107d28031f1ff2c0c00e1600d677cdf6b2
Yao <ydwang@icapitalnetwork.com>
2024-06-09 22:42:43 -0400

    first init

---
+++ model/model.py
@@ -0,0 +1,39 @@
+"""
+The `Model` class is an interface between the ML model that you're packaging and the model
+server that you're running it on.
+
+The main methods to implement here are:
+* `load`: runs exactly once when the model server is spun up or patched and loads the
+   model onto the model server. Include any logic for initializing your model, such
+   as downloading model weights and loading the model into memory.
+* `predict`: runs every time the model server is called. Include any logic for model
+  inference and return the model output.
+
+See https://truss.baseten.co/quickstart for more.
+"""
+from .src.test import TestScene
+
+
+class Model:
+    def __init__(self, **kwargs):
+        # Uncomment the following to get access
+        # to various parts of the Truss config.
+
+        # self._data_dir = kwargs["data_dir"]
+        # self._config = kwargs["config"]
+        # self._secrets = kwargs["secrets"]
+        self._model = None
+
+    def load(self):
+        # Load model here and assign to self._model.
+        self._modal = TestScene()
+
+    def predict(self, model_input):
+        # Run model inference here
+        action = model_input["action"]
+        input = model_input["input"]
+        if action=="echo_name":
+            return self._modal.echo_name(input)
+        if action == "odd_even":
+            return self._modal.odd_even(input)
+
---
+++ model/src/test.py
@@ -0,0 +1,14 @@
+class TestScene:
+  """Analyzes a number to determine if it's odd or even."""
+
+  def odd_even(self, number: int):
+    if number % 2 == 0:
+      return "Even"
+    else:
+      return "Odd"
+
+  def echo_name(self, name: str):
+    return f"Hello, {name}?"
+
+
+# ULgylOGO.Fw3Y2rhuer2Ke0Skghu7sQAGX4CnJwVV
